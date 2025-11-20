d394d4b9d09f4a13473256fdb2e16b3301e0ccce
Hillary Lechuga <hillarylechuga87@gmail.com>
2025-11-20 09:38:18 -0500

    Eliminar conexión duplicada y usar conectarDB()

--- server.js
+++ server.js
@@ -11,16 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Rutas
 const resenasRoutes = require("./routes/resena");
 app.use("/api/resenas", resenasRoutes);
 
-// Conexión a Mongo
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Conectado a MongoDB"))
-  .catch((err) => console.error("❌ Error de conexión:", err));
-
+import conectarDB from "./config/db.js";
+conectarDB();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Servidor en puerto ${PORT}`));
  
